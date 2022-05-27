2c36fca615527165c78edefe3dc98849bc0d2451
Nina Jiang <njiang20@stuy.edu>
2022-05-27 13:16:13 -0400

    woooo

---
+++ v0/Player.java
@@ -0,0 +1,42 @@
+public Player {
+  private String name;
+  private int kills, deaths, acs, adr, kast;
+
+  public Player(String n, int k, int d, int ac, int ad, int k) {
+    name = n;
+    kills = k;
+    deaths = d;
+    acs = ac;
+    adr = ad;
+    kast = k;
+  }
+  
+  public String getName() {
+
+  }
+
+  public int getKills() {
+
+  }
+
+  public int getDeaths() {
+
+  }
+
+  public int getACS() {
+
+  }
+
+  public int getADR() {
+
+  }
+
+  public int getKAST() {
+
+  }
+
+  public String toString() {
+    return "";
+  }
+
+}
