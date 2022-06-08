195dd27f1833b01bcb3f4e9cab56c3047505027c
Lea Kwok <lea.a.kwok@gmail.com>
2022-06-08 02:52:27 -0400

    yay

--- v6/test.java
+++ v6/test.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class test{
     public static void main(String[] args){
-        DataSource ds = DataSource.connect(VCTmastersv1.csv);
+        DataSource ds = DataSource.connect("./VCTmastersv1.csv");
         ds.load();
         ds.printUsageString();
     }
